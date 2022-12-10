447d52460603d34ba2074e71708a0f18ecdd024c
iiina <iiina@DESKTOP-A4R70HK>
2022-12-10 20:29:47 +0900

    초기 커밋.

---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+           
+            if(e.KeyCode == Keys.F1&&e.Shift)
+            {
+                MessageBox.Show("F1+Shift");
+            }
+            if (e.KeyCode == Keys.F1 && e.Control)
+            {
+                MessageBox.Show("F1+Ctrl");
+            }
+            if (e.KeyCode == Keys.F1 && e.Alt)
+            {
+                MessageBox.Show("F1+Alt");
+            }
+            if (e.KeyCode == Keys.F1 )
+            {
+                MessageBox.Show("F1");
+            }
+        }
+    }
+}
---
+++ WindowsFormsApp8/Form1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp8
+{
+    public partial class Form1 : Form
+    {
+        Rectangle rectMouse;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+           
+
+            e.Graphics.DrawRectangle(Pens.Black, rectMouse);
+
+           
+        }
+
+        private void Form1_MouseDown(object sender, MouseEventArgs e)
+        {
+            rectMouse.X = e.X;
+
+            rectMouse.Y = e.Y;
+
+            rectMouse.Width = 0;
+
+            rectMouse.Height = 0;
+
+            Invalidate();
+        }
+
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+
+            {
+
+                rectMouse.Width = e.X - rectMouse.X + 1; 
+                rectMouse.Height = e.Y - rectMouse.Y + 1; 
+
+                Invalidate(); 
+            }
+        }
+    }
+}
